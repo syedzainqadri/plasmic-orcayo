b07f97704331ce6c897d72a2be3f53d32bf18154
SyedQadri <96788793+syedzainqadri@users.noreply.github.com>
2025-11-14 20:11:47 +0500

    lets test updated db configuration

--- platform/wab/ormconfig.js
+++ platform/wab/ormconfig.js
@@ -1,25 +1,24 @@
 module.exports = {
-  type: "postgres",
-  host: process.env.WAB_DBHOST || "142.44.136.233",
-  username: process.env.WAB_DBUSER || "wab",
-  database: process.env.WAB_DBNAME || "plasmic-db",
-  password: process.env.WAB_DBPASSWORD || "SEKRET",
-  port: parseInt(process.env.WAB_DBPORT || "5433", 10),
-  synchronize: false,
-  dropSchema: false,
-  logging: false,
-  entities: [
+  "type": "postgres",
+  "host": "plasmic-db",
+  "username": "wab",
+  "database": "wab",
+  "password": "SEKRET",
+  "synchronize": false,
+  "dropSchema": false,
+  "logging": false,
+  "entities": [
     "src/wab/server/entities/**/*.ts"
   ],
-  migrations: [
+  "migrations": [
     "src/wab/server/migrations/**/*.ts"
   ],
-  subscribers: [
+  "subscribers": [
     "src/wab/server/subscribers/**/*.ts"
   ],
-  cli: {
-    entitiesDir: "src/wab/server/entities",
-    migrationsDir: "src/wab/server/migrations",
-    subscribersDir: "src/wab/server/subscribers"
+  "cli": {
+    "entitiesDir": "src/wab/server/entities",
+    "migrationsDir": "src/wab/server/migrations",
+    "subscribersDir": "src/wab/server/subscribers"
   }
 };
