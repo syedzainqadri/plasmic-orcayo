9913b5c37fa889ce235e4e1fda37ed472920e542
SyedQadri <96788793+syedzainqadri@users.noreply.github.com>
2025-11-16 18:46:29 +0500

    providing data source

---
+++ platform/wab/data-source.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm";
+
+export default new DataSource({
+  type: "postgres",
+  host: process.env.WAB_DBHOST || process.env.DB_HOST || "plasmic-db",
+  port: parseInt(process.env.WAB_DBPORT || process.env.DB_PORT || "5432"),
+  username: process.env.WAB_DBUSER || process.env.DB_USER || "wab",
+  password: process.env.WAB_DBPASSWORD || process.env.DB_PASS || "SEKRET",
+  database: process.env.WAB_DBNAME || process.env.DB_NAME || "wab",
+  synchronize: false,
+  dropSchema: false,
+  logging: false,
+  entities: [
+    "src/wab/server/entities/**/*.ts"
+  ],
+  migrations: [
+    "src/wab/server/migrations/**/*.ts"
+  ],
+  subscribers: [
+    "src/wab/server/subscribers/**/*.ts"
+  ]
+});
